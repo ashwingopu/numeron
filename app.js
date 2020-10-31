88a4d4ac3d5209a99c18c6c9f73d59c4c495607d
ashwingopu <67531553+ashwingopu@users.noreply.github.com>
2020-10-31 15:49:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const playbutton=document.getElementById("play-button")
+
+playButton.onclick=()=>{
+    location.href="./game.html"
+}
---
+++ score.js
@@ -0,0 +1,19 @@
+var urlParams=new URLSearchParams(window.location.search);
+
+var score=urlParams.get("score");
+var scoreBoard=document.getElementById("score-board");
+scoreBoard.innerHTML=score;
+
+
+
+var urlParams=new URLSearchParams(window.location.search);
+
+var score=urlParams.get("score");
+var scoreBoard=document.getElementById("score-board");
+scoreBoard.innerHTML=score;
+
+var playAgain=document.getElementById("play-again-button");
+
+playAgain.onclick=()=>{
+    location.href="./game.html";
+}
